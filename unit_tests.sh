32bf1786ae76088e8138cc7949a9a2d19feb2b49
Snild Dolkow <snild@dolkow.se>
2019-08-26 20:35:24 +0200

    report coverage of unit tests
    I want to have 100% test coverage.

--- unit_tests.sh
+++ unit_tests.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-python3 -m unittest discover test.unit
+set -e
+
+python3-coverage run --branch --include 'hprof/*' -m unittest discover test.unit
+python3-coverage report -m --fail-under=100
