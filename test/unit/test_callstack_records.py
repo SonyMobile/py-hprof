ebe8782f34ec87e89eca4c968e70c527a67c3a43
Snild Dolkow <snild@dolkow.se>
2019-08-30 20:06:29 +0200

    add utils for running tests with varying idsize
    ...and use them in the callstack parsing tests.

--- test/unit/test_callstack_records.py
+++ test/unit/test_callstack_records.py
@@ -1,20 +1,22 @@
 import unittest
 import hprof
 
-from .util import dehex
+from .util import varyingid
 
+@varyingid
 class TestParseStackTraceRecords(unittest.TestCase):
 
 	def setUp(self):
 		self.hf = hprof._parsing.HprofFile()
-		self.hf.idsize = 4
-		self.hf.names[0x80104030] = 'hello'
-		self.hf.names[0x55555555] = 'five'
-		self.hf.names[0x08070605] = 'dec'
-		self.hf.names[0x10] = 'sixteen'
-		self.hf.names[0x11] = 'moreThanSixteen'
-		self.hf.names[0xdead] = '()V'
-		self.hf.names[0xf00d] = '(Ljava/lang/String;)I'
+		self.hf.idsize = self.idsize
+		self.hf.names[self.id(0x82104030)] = 'hello'
+		self.hf.names[self.id(0x55555555)] = 'five'
+		self.hf.names[self.id(0x08070605)] = 'dec'
+		self.hf.names[self.id(0x10)] = 'sixteen'
+		self.hf.names[self.id(0x11)] = 'moreThanSixteen'
+		self.hf.names[self.id(0xdead)] = '()V'
+		self.hf.names[self.id(0xf00d)] = '(Ljava/lang/String;)I'
+		assert len(self.hf.names) == 8, repr(self.hf.names) # no dupes.
 
 	def addframe(self, indata):
 		reader = hprof._parsing.PrimitiveReader(memoryview(indata))
@@ -25,9 +27,12 @@ class TestParseStackTraceRecords(unittest.TestCase):
 		hprof._parsing.parse_stack_frame_record(self.hf, reader)
 
 	def test_frame_only(self):
-		self.addframe(dehex('02030405 55555555 0000dead 80104030 12345678 00000051'))
-		self.assertIn(0x02030405, self.hf.stackframes)
-		frame = self.hf.stackframes[0x02030405]
+		self.addframe(self.build()
+				.id(0x02030405).id(0x55555555).id(0xdead)
+				.id(0x82104030).u4(0x12345678).i4(0x51))
+		fid = self.id(0x02030405)
+		self.assertIn(fid, self.hf.stackframes)
+		frame = self.hf.stackframes[fid]
 		self.assertIsInstance(frame, hprof.callstack.Frame)
 		self.assertEqual(frame.method, 'five')
 		self.assertEqual(frame.signature, '()V')
@@ -36,9 +41,12 @@ class TestParseStackTraceRecords(unittest.TestCase):
 		self.assertEqual(frame.line, 0x51)
 
 	def test_negative_line(self):
-		self.addframe(dehex('02030405 55555555 0000dead 80104030 12345678 fffffffe'))
-		self.assertIn(0x02030405, self.hf.stackframes)
-		frame = self.hf.stackframes[0x02030405]
+		self.addframe(self.build()
+				.id(0x02030405).id(0x55555555).id(0xdead)
+				.id(0x82104030).u4(0x12345678).i4(-2))
+		fid = self.id(0x02030405)
+		self.assertIn(fid, self.hf.stackframes)
+		frame = self.hf.stackframes[fid]
 		self.assertIsInstance(frame, hprof.callstack.Frame)
 		self.assertEqual(frame.method, 'five')
 		self.assertEqual(frame.signature, '()V')
@@ -47,24 +55,34 @@ class TestParseStackTraceRecords(unittest.TestCase):
 		self.assertEqual(frame.line, -2)
 
 	def test_duplicate_id(self):
-		self.addframe(dehex('02030405 55555555 0000dead 80104030 12345678 00000051'))
+		self.addframe(self.build()
+				.id(0x02030405).id(0x55555555).id(0xdead)
+				.id(0x82104030).u4(0x12345678).i4(0x51))
 		with self.assertRaisesRegex(hprof.error.FormatError, 'duplicate'):
-			self.addframe(dehex('02030405 55555555 0000dead 80104030 12345678 fffffffe'))
+			self.addframe(self.build()
+					.id(0x02030405).id(0x55555555).id(0xdead)
+					.id(0x82104030).u4(0x12345678).i4(-2))
 
 	def test_multiple_frames(self):
-		self.addframe(dehex('03030404 80104030 0000f00d 00000011 12345678 ffffffff'))
-		self.addframe(dehex('03030407 00000010 55555555 08070605 22345678 00000171'))
-		self.assertIn(0x03030404, self.hf.stackframes)
-		self.assertIn(0x03030407, self.hf.stackframes)
+		self.addframe(self.build()
+				.id(0x03030404).id(0x82104030).id(0xf00d)
+				.id(0x00000011).u4(0x12345678).i4(-1))
+		self.addframe(self.build()
+				.id(0x03030407).id(0x00000010).id(0x55555555)
+				.id(0x08070605).u4(0x22345678).i4(0x171))
+		fid1 = self.id(0x03030404)
+		fid2 = self.id(0x03030407)
+		self.assertIn(fid1, self.hf.stackframes)
+		self.assertIn(fid2, self.hf.stackframes)
 
-		frame = self.hf.stackframes[0x03030404]
+		frame = self.hf.stackframes[fid1]
 		self.assertIsInstance(frame, hprof.callstack.Frame)
 		self.assertEqual(frame.method, 'hello')
 		self.assertEqual(frame.signature, '(Ljava/lang/String;)I')
 		self.assertEqual(frame.sourcefile, 'moreThanSixteen')
 		self.assertEqual(frame.line, -1)
 
-		frame = self.hf.stackframes[0x03030407]
+		frame = self.hf.stackframes[fid2]
 		self.assertIsInstance(frame, hprof.callstack.Frame)
 		self.assertEqual(frame.method, 'sixteen')
 		self.assertEqual(frame.signature, 'five')
--- test/unit/util.py
+++ test/unit/util.py
@@ -1,3 +1,67 @@
-def dehex(s):
-	s = s.replace(' ', '')
-	return bytes(int(s[x:x+2], 16) for x in range(0, len(s), 2))
+import unittest
+
+from struct import pack
+
+def fold(val, bytes):
+	if val < 0:
+		raise ValueError('%d is less than zero' % val)
+	folded = 0
+	while val != 0:
+		folded ^= val
+		val >>= 8 * bytes
+	mask = (1 << 8 * bytes) - 1
+	return folded & mask
+
+def varyingid(cls):
+	''' test class decorator that wraps all test functions
+	    in subtest loops with different id sizes '''
+	def wrap(fn):
+		def idsize_loop(self, *args, **kwargs):
+			for idsize in (3,4,5):
+				with self.subTest(idsize=idsize):
+					self.idsize = idsize
+					self.setUp()
+					fn(self, *args, **kwargs)
+		return idsize_loop
+	for name in cls.__dict__:
+		if name.startswith('test_'):
+			fn = getattr(cls, name)
+			if callable(fn):
+				wrapped = wrap(fn)
+				wrapped.__name__ = fn.__name__
+				setattr(cls, name, wrapped)
+	cls.idsize = 4 # just need something for the initial setUp call
+
+	def build(self):
+		return Builder(self.idsize)
+	cls.build = build
+
+	def id(self, val):
+		return fold(val, self.idsize)
+	cls.id = id
+
+	return cls
+
+
+class Builder(bytearray):
+	def __init__(self, idsize):
+		self.idsize = idsize
+
+	def u(self, val, bytes):
+		folded = fold(val, bytes)
+		bs = folded.to_bytes(bytes, 'big')
+		self.extend(bs)
+		return self
+
+	def i(self, val, bytes):
+		mask = (1 << 8 * bytes) - 1
+		return self.u(val & mask, bytes)
+
+	def u4(self, val):
+		return self.u(val, 4)
+
+	def i4(self, val):
+		return self.i(val, 4)
+
+	def id(self, val):
+		return self.u(val, self.idsize)
